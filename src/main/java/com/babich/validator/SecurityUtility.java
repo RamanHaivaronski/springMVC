d9ff0793f32254cfa379468dbb6be2a8d25fca3e
RamanHaivaronski <romangaivoronsky@gmail.com>
2018-02-26 17:51:05 +0300

    rn

--- src/main/java/com/babich/validator/SecurityUtility.java
+++ src/main/java/com/babich/validator/SecurityUtility.java
@@ -19,14 +19,14 @@ public class SecurityUtility {
     @Bean
     public static String randomPassword(){
         String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-        StringBuilder salt = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         Random rnd = new Random();
 
-        while (salt.length() < 18) {
+        while (sb.length() < 18) {
             int index = (int) (rnd.nextFloat()*SALTCHARS.length());
-            salt.append(SALTCHARS.charAt(index));
+            sb.append(SALTCHARS.charAt(index));
         }
-        String saltStr = salt.toString();
+        String saltStr = sb.toString();
         return saltStr;
     }
 
