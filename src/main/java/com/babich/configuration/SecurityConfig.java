963878ffd2f282d105b87f4f441949cdbe802044
RamanHaivaronski <romangaivoronsky@gmail.com>
2018-02-26 17:34:14 +0300

    BCrypt Salt Encoding added

--- src/main/java/com/babich/configuration/SecurityConfig.java
+++ src/main/java/com/babich/configuration/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.babich.configuration;
 
 
+import com.babich.validator.SecurityUtility;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -9,7 +11,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import com.babich.services.UserDetailsServiceImpl;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Configuration
 @EnableWebSecurity
@@ -19,9 +22,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsServiceImpl userDetailsServiceImpl;
 
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsServiceImpl);
+        auth.userDetailsService(userDetailsServiceImpl).passwordEncoder(passwordEncoder());
     }
 
     @Override
@@ -35,5 +39,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         ;
 
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return SecurityUtility.passwordEncoder();
+    }
+
+
 
 }
